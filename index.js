b5003ef5651d0aab0728c8a22d16b28ebcc14bd4
voste126 <steveaustine126@gmail.com>
2023-06-15 18:23:57 +0300

    maps

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    //Inside the callback function, the tutorial name is split into an array of words using the split(' ') method. The space character is used as the separator.
+  //The map() method is then used on the words array to create a new array based on the provided callback function.
+//Inside the second callback function, word.charAt(0).toUpperCase() is used to capitalize the first letter of each word. word.charAt(0) retrieves the first character of the word, and toUpperCase() converts it to uppercase.
+//  The word.slice(1) method is used to retrieve the remaining characters of each word, starting from index 1. This effectively removes the first character from each word.
+//The result is an array of capitalized words.
+//Finally, capitalizedWords.join(' ') joins the capitalized words into a single string, separated by a space.
+//The map() method continues iterating over each tutorial name, performing the same transformations, and returns a new array with the title case tutorial names.
+    const words = tutorial.split(' ');
+    const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return capitalizedWords.join(' ');
+  });
 }
